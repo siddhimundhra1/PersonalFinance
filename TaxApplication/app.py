7951c86dfa417010bb66796584526d188c99c43f
Siddhi Mundhra <siddhimundhra@ucsb.edu>
2025-08-25 12:26:42 -0700

    First commit

---
+++ TaxApplication/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, request, redirect, render_template
+
+#CONFIGURATION
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
